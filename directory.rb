7195ff45f434197017255a148441e6cc186091c6
Jacob Cho <jacobcho@live.com>
2021-09-18 21:02:52 +0100

    finished ex 1-3

--- directory.rb
+++ directory.rb
@@ -1,3 +1,10 @@
+class String
+  def initial # Ruby's open classes, allows initial method on any string
+    self[0,1]
+  end
+end
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice" # empty name returns final array
@@ -8,23 +15,31 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
-    name = gets.chomp
+    if name.length > 11
+      puts "Enter a name shorter than 12 characters"
+      name = gets.chomp
+    elsif name.initial == "a" || name.initial == "A"
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      name = gets.chomp  #get another name from user 
+    else
+      puts "Enter a name with the letter 'A'"
+      name = gets.chomp
+    end
+
   end
   # returns the final array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The students of Villains Academy, whose names begin with 'A'"
   puts "---------------"
 end
 
 def print(students)
-  students.each do |student_el|
-    puts "#{student_el[:name]} (#{student_el[:cohort]} cohort)"
+  students.each_with_index do |student_el, index|
+    puts "#{index + 1}. #{student_el[:name]} (#{student_el[:cohort]} cohort)"
   end
 end
 
@@ -33,7 +48,7 @@ def print_footer(students)
 end
 
 # now let's call the methods
-students = input_students # gets students from user
+students = input_students # gets students from usersf
 print_header
 print(students) # array goes into def print
-print_footer(students) # array goes into def print_footer
+print_footer(students) # array goes into def print_footerhjlk
